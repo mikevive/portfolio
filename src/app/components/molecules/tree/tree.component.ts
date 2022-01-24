5452d0bab4d7296cccd45a6a9c46cf4d41785e8e
mikevive <mikevive327@gmail.com>
2022-01-24 04:19:19 -0500

    Add Chromatic Aberration and Motion Blur
    In order to add more style to de elements on the page a chromatic aberration is added using RGB clones of the elements. also, Motion Blur is added to the tress when scrolling.

--- src/app/components/molecules/tree/tree.component.ts
+++ src/app/components/molecules/tree/tree.component.ts
@@ -8,27 +8,48 @@ import { TreeService } from './tree.service';
 })
 export class TreeComponent implements OnInit {
 
+  @ViewChild('blue', { static: true }) blueRef: ElementRef;
+  @ViewChild('red', { static: true }) redRef: ElementRef;
+  @ViewChild('green', { static: true }) greenRef: ElementRef;
+
   @Input() position: string;
   top: number;
   left: number;
   opacity: number;
+  blurOpacity: number;
+
+  scroll: number = 0;
+  scrollTime: any;
 
   private opacities: any = {
     base: 0.2,
     mid: 0.5,
-    top: 1
+    top: 0.8
+  }
+
+  private blurOpacities: any = {
+    base: 0.1,
+    mid: 0.2,
+    top: 0.3
+  }
+
+  private blurTranslations: any = {
+    base: 6,
+    mid: 4,
+    top: 2
   }
 
   constructor(
     public elementRef: ElementRef,
     public treeService: TreeService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     const parentRef = this.elementRef.nativeElement.parentElement;
     const parentWidth: number = parentRef.clientWidth;
     const parentHeight: number = parentRef.clientHeight;
     this.opacity = this.opacities[this.position];
+    this.blurOpacity = this.blurOpacities[this.position];
 
     let isRegister: boolean;
 
@@ -40,6 +61,48 @@ export class TreeComponent implements OnInit {
       this.left = left;
     }
     while(!isRegister)
+
+    const blurTranslation = this.blurTranslations[this.position]
+
+    this.blueRef.nativeElement.style.transform = `translateX(-${blurTranslation}px) translateY(-${blurTranslation}px)`
+    this.redRef.nativeElement.style.transform = `translateX(${blurTranslation}px) translateY(${blurTranslation}px)`
+    this.greenRef.nativeElement.style.transform = `translateX(-${blurTranslation}px) translateY(${blurTranslation}px)`
+
+    document.getElementById('parallax-wrapper').addEventListener(
+      'scroll',
+      () => {
+
+          this.scrollTime = Date.now();
+
+          // TODO: remove document selector
+          const scrollTop = document.getElementById('parallax-wrapper').scrollTop;
+          const diff = this.scroll - scrollTop
+          this.scroll = scrollTop
+
+          const treeElements= this.elementRef.nativeElement.getElementsByClassName('tree')
+          for (let treeElement of treeElements) {
+            treeElement.style.height = `${100 + Math.abs(diff)}px`;
+            if(diff > 0) {
+              treeElement.style.top = `${this.top - diff}px`
+            };
+          }
+
+          setTimeout(() => {
+            const currentTime = Date.now();
+            const timeDiff = currentTime - this.scrollTime;
+            console.log('timeDiff: ', currentTime - this.scrollTime)
+            if(timeDiff >= 100){
+              const treeElements= this.elementRef.nativeElement.getElementsByClassName('tree')
+              for (let treeElement of treeElements) {
+                treeElement.style.height = `100px`;
+                treeElement.style.top = `${this.top}px`
+              }
+            }
+
+          }, 100 )
+      },
+      false
+    )
   }
 
 }
