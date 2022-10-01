c7bb3f067d2c92dfec374a92c6f6457db9f15118
mikevive <mikevive327@gmail.com>
2022-10-01 14:27:43 -0500

    refactor(presentation): simplify moon floating animation

--- src/app/components/organisms/presentation/presentation.component.ts
+++ src/app/components/organisms/presentation/presentation.component.ts
@@ -15,38 +15,13 @@ import { SplashService } from '../../pages/public/splash/splash.service';
   styleUrls: ['./presentation.component.scss'],
 })
 /** TODO: Comment */
-export class PresentationComponent implements OnInit, AfterViewInit {
+export class PresentationComponent implements AfterViewInit {
   @ViewChildren('loadImg')
   loadImg!: QueryList<ElementRef>;
 
-  @ViewChild('moonSubcontainer', { static: true })
-  moonSubcontainerRef!: ElementRef;
-
   img: string = 'assets/img/logo.png';
 
-  top: number = 0;
-  topMax: number = 5;
-  topMin: number = -5;
-  direction: string = 'UP';
-
   constructor(public splashService: SplashService) {}
-
-  /**
-   * @returns Void.
-   */
-  ngOnInit(): void {
-    setInterval(() => {
-      const moonSubcontainer = this.moonSubcontainerRef.nativeElement;
-      if (this.direction === 'UP') {
-        this.top = this.top + 1;
-        if (this.top >= this.topMax) this.direction = 'DOWN';
-      } else {
-        this.top = this.top - 1;
-        if (this.top <= this.topMin) this.direction = 'UP';
-      }
-      moonSubcontainer.style.top = `${this.top}px`;
-    }, 200);
-  }
 
   ngAfterViewInit(): void {
     const numberOfImagesBeingLoaded = this.loadImg.length;
